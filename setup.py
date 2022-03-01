960add7da29b7577e3a0c1cf39f3695bb1fc5fce
grisuthedragon <grisuthedragon@users.noreply.github.com>
2022-03-01 12:58:55 +0100

    Ongoing

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
+#!/usr/bin/env python3
+
 from setuptools import setup
 
 setup(
    name='pypodget',
    version='0.1.0',
    author='Martin Koehler',
-   author_email='aac@example.com',
+   author_email='grisuthedragon@users.noreply.github.com',
    scripts=['bin/pypodget'],
-   url='http://pypi.python.org/pypi/PackageName/',
+   url='http://github.com/grisuthedraong/pypodget',
    license='LICENSE.txt',
-   description='An awesome package that does something',
-   long_description='open(README.txt).read()',
+   description='A small tool to download podcasts from RSS feeds.',
+   long_description=open('README.md').read(),
    install_requires=[
        "requests >= 2.22",
-	   "tqdm >= 4.30"
+	   "tqdm >= 4.30",
+       "eyed3 >= 0.8.10"
    ],
+   package_data ={'': ['pods.ini.example']},
+   include_package_data=True
 )
 
