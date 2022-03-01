92c9df5dcd920c4277087c8186a2df49f94b3062
grisuthedragon <grisuthedragon@users.noreply.github.com>
2022-03-01 22:43:55 +0100

    Update MANIFEST and setup.py

--- setup.py
+++ setup.py
@@ -9,15 +9,24 @@ setup(
    author_email='grisuthedragon@users.noreply.github.com',
    scripts=['bin/pypodget'],
    url='http://github.com/grisuthedraong/pypodget',
-   license='LICENSE.txt',
+   license=('LICENSE.txt',),
    description='A small tool to download podcasts from RSS feeds.',
    long_description=open('README.md').read(),
+   python_requires='>3.7.0',
    install_requires=[
        "requests >= 2.22",
 	   "tqdm >= 4.30",
        "eyed3 >= 0.8.10"
    ],
    package_data ={'': ['pods.ini.example']},
-   include_package_data=True
+   include_package_data=True,
+   classifiers = [
+       'Development Status :: 2 - Pre-Alpha',
+       'Environment :: Console',
+       'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+       'Operating System :: OS Independent',
+       'Programming Language :: Python :: 3',
+       'Topic :: Multimedia :: Sound/Audio',
+       ]
 )
 
